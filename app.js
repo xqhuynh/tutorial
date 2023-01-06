714f1a9d671a441ee8dd2c675f03b6dc440f53a8
Savon Huynh <xqhuynh@gmail.com>
2023-01-05 17:08:20 -0800

    Basic tutorial

---
+++ app.js
@@ -0,0 +1,11 @@
+document.write('Hello world');
+console.log('hello there');
+console.log('top of the morning');
+let name = 'Savon';
+
+// template literals using backticks
+console.log(`Hi ${name}`);
+
+let firstName = 'Nina';
+const lastName = 'Huynh';
+console.log(firstName, lastName);
